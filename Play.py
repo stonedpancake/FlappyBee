7ae5ca4cc8e77e103aa9f5ec2eb88a1f5bed5e09
Вячеслав <slava2005qwe@gmail.com>
2020-12-31 02:30:39 +0300

    ADDED: columns moving, columns physic (not all)

--- Play.py
+++ Play.py
@@ -1,3 +1,5 @@
+from random import choice
+
 import pygame
 from pygame import init, display, image, Rect, time, QUIT, MOUSEBUTTONDOWN, Surface, transform, KEYDOWN, K_RIGHT, K_LEFT, K_SPACE, draw
 
@@ -14,44 +16,62 @@ class Play:
         self.main_screen = display.set_mode(SIZE)
         self.surface = Surface(SF_SIZE)
 
+        self.start = False
+
         self.bg_picture = image.load("./Data/Pictures/bg_retrowave.jpg")
         self.sprite_img = image.load("./Data/Sprites/bee2.png")
-
         self.columns = image.load("./Data/Pictures/Column.png")
-
-        self.column_gap = 120  # self.sprite.get_height()
-        self.column_cut = 40  # CHANGES COLUMN GAP POSITION
-
-        self.column_x = 1050
-        self.column_y = 0
-
-        self.column_height = 444 + self.column_cut
-        self.upd_column_y = self.column_y + self.column_height + self.column_gap
-
-        self.column = Rect(90, 1000, 90, self.column_height)
-        self.upd_column = Rect(190, 1000 + self.column_cut, 90, HEIGHT - self.upd_column_y + 10)
 
         self.sprite_x = 50
         self.sprite_y = 470
         self.sprite_width = self.sprite_img.get_width() - 12
         self.sprite_height = self.sprite_img.get_height() - 12
 
+        self.column_gap = 200  # CHANGES COLUMN GAP SIZE
+        self.column_cut = 0  # CHANGES COLUMN GAP POSITION
+
+        self.column_x = 2200
+        self.column_y = 0
+
+        self.column_height = 450 + self.column_cut
+        self.upd_column_y = self.column_y + self.column_height + self.column_gap
+
+        self.column = Rect(90, 1000, 90, self.column_height)
+        self.upd_column = Rect(190, 1000 + self.column_cut, 90, HEIGHT - self.upd_column_y + 10)
+
         self.column_box = Rect(self.column_x, self.column_y, self.column.width, self.column.height)
         self.upd_column_box = Rect(self.column_x, self.upd_column_y, self.upd_column.width, self.upd_column.height)
 
-        self.bg_move = 1920
+        # self.bg_move = ((self.sprite_x + self.sprite_width) // 1920) * 1920
 
         # COLUMN AND SPRITES PHYSIC
 
     def main(self):
 
-        self.surface.blit(self.bg_picture, (self.bg_move, 0))
-
+        self.surface.blit(self.bg_picture, (0, 0))
         self.surface.blit(self.sprite_img, (self.sprite_x, self.sprite_y))
+
         self.sprite_box = Rect(self.sprite_x + 5, self.sprite_y, self.sprite_width, self.sprite_height)
 
-        self.surface.blit(self.columns, (self.column_x, self.column_y), self.column)
-        self.surface.blit(self.columns, (self.column_x, self.upd_column_y), self.upd_column)
+        # DRAW COLUMNS
+
+        if self.move() and self.start:
+
+            self.column_x -= 3  # CHANGES COLUMN SPEED
+
+            if self.column_x >= 0:
+
+                self.surface.blit(self.columns, (self.column_x, self.column_y), self.column)
+                self.surface.blit(self.columns, (self.column_x, self.upd_column_y), self.upd_column)
+
+            else:
+
+                self.column_x = 1920
+
+            self.column_box = Rect(self.column_x, self.column_y, self.column.width, self.column.height)
+            self.upd_column_box = Rect(self.column_x, self.upd_column_y, self.upd_column.width, self.upd_column.height)
+
+        # DRAW COLUMNS
 
         # DRAW BOXES
 
@@ -62,6 +82,7 @@ class Play:
         # DRAW BOXES
 
         self.main_screen.blit(self.surface, (0, 0))
+        # time.delay(100)
         display.flip()
 
     def update_box(self, box):
@@ -81,10 +102,10 @@ class Play:
     def run(self):
 
         exit_ = False
-        clock = time.Clock()
+        self.clock = time.Clock()
 
         while not exit_:
-            clock.tick(60)
+            self.clock.tick(60)
 
             self.main()
             display.flip()
@@ -99,14 +120,12 @@ class Play:
                     if event.key == K_RIGHT and self.move():
 
                         self.sprite_x += 10
-                        self.bg_move -= 100
 
                     if event.key == K_LEFT and self.move():
 
                         self.sprite_x -= 10
-                        self.bg_move += 100
 
                     if event.key == K_SPACE:
 
-                        self.sprite_y -= 10
+                        self.start = True
 
